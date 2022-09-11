def62deaa7523a5d8156e5e1797f9a3818f02f6b
Unique Shrestha <112570655+Unique-py@users.noreply.github.com>
2022-09-11 21:34:56 +0545

    Create Write-a-program-to-Print-I-love-you-10000000000000000000000000000000000000000000000000000000-times.cpp

---
+++ Write-a-program-to-Print-I-love-you-10000000000000000000000000000000000000000000000000000000-times.cpp
@@ -0,0 +1,9 @@
+#include
+#include
+using namespace std;
+int main()
+{ 
+int i;
+for(i=1;i<=10000000000000000000000000000000000000000000000000000000;++i)
+cout<<"I love you 3000"<<"\n"; return 0;
+} 
